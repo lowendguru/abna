310bbcc07b91ffa20284744065bd2dcccbfc1ab3
Diego Bruno <lowendguru@gmail.com>
2022-07-04 01:40:29 +0200

    removing unused code

--- src/test/java/functionaltests/pageobjects/BasePage.java
+++ src/test/java/functionaltests/pageobjects/BasePage.java
@@ -17,30 +17,5 @@ import java.time.Duration;
 public class BasePage extends PageObject {
     final static Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
-    public static void clickByScript(WebDriver driver, WebElement element) {
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("arguments[0].click();", element);
-    }
-
-
-    public boolean waitForJStoLoad() {
-        return waitForJStoLoad(30);
-    }
-
-    public boolean waitForJStoLoad(int timeoutSeconds) {
-        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(timeoutSeconds));
-        // wait for jQuery to load
-        ExpectedCondition<Boolean> jQueryLoad = driver -> {
-            try {
-                return ((Long) ((JavascriptExecutor) driver).executeScript("return jQuery.active") == 0);
-            } catch (Exception e) {
-                return true;
-            }
-        };
-        // wait for Javascript to load
-        ExpectedCondition<Boolean> jsLoad = driver -> ((JavascriptExecutor) driver)
-                .executeScript("return document.readyState").toString().equals("complete");
-        return wait.until(jQueryLoad) && wait.until(jsLoad);
-    }
 
 }
